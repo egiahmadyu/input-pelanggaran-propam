d8cc3cc65e300a8c70b58a4ada1f62f501ac6a97
Ahmad Rizky Prayogi <a.rizkyprayogi@gmail.com>
2023-09-25 13:50:28 +0700

    e

--- resources/views/content/pelanggaran/tambah.blade.php
+++ resources/views/content/pelanggaran/tambah.blade.php
@@ -419,7 +419,7 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title">Modal Check</h5>
+                    <h5 class="modal-title">Check Pelanggar</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
